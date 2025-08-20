054fcfc7dd1235f9fe09132d1ca43034dcc9601c
Christian Himmler <christian@himmlers.de>
2025-08-20 23:06:05 +0200

    pylint

--- app/models/user.py
+++ app/models/user.py
@@ -3,6 +3,7 @@ from ..database import Base
 
 class User(Base):
     __tablename__ = 'users'
+
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
